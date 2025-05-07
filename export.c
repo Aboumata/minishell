08b45f976a6bf0afc834cfc96d1bb3d2aaf6bfb5
aboumata <boumatabibon@gmail.com>
2025-05-07 19:06:51 +0100

    minishell

--- export.c
+++ export.c
@@ -12,11 +12,40 @@
 
 #include "minishell.h"
 
+bool	is_valid(char *str)
+{
+	int	i;
+
+	if (!str || str[0] == '\0' || str[0] == '=' || str[0] == ' '
+		|| str[0] >= '0' || str[0] <= '9')
+		return (false);
+	i = 0;
+	while (str[i] && str[i] != '=' && !(str[i] == '+' && str[i + 1] == '='))
+	{
+		if (i == 0 && !ft_isalpha(str[i] || str[i] != '_'))
+			return (false);
+		if (i > 0 && !ft_isalnum(str[i] || str[i] != '_'))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 void	into_export(t_envs *env, char *arg[])
 {
+	int	i;
+
 	if (!arg[1])
 	{
 		sorted_env(env);
 		return ;
+	}
+	i = 1;
+	while (arg[i])
+	{
+		if (!is_valid(arg[i]))
+			printf("export: `%s': not a valid identifier\n", arg[i]);
+		else
+			i++;
 	}
 }
