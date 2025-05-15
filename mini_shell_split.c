fbdd7b83b0e5e98dbfa2aa5d53dcee60a8fb10e5
aboumata <boumatabibon@gmail.com>
2025-05-15 11:36:05 +0100

    minishell

--- mini_shell_split.c
+++ mini_shell_split.c
@@ -1,5 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mini_shell_split.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboumata <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 16:40:39 by aboumata          #+#    #+#             */
+/*   Updated: 2025/05/14 16:40:41 by aboumata         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
-#include <stdlib.h>
 
 static void	skip_spaces(const char *s, int *i)
 {
@@ -7,90 +18,81 @@ static void	skip_spaces(const char *s, int *i)
 		(*i)++;
 }
 
-static int	count_quoted_token(const char *s, int *i)
-{
-	int		count;
-	char	quote;
-
-	count = 0;
-	quote = s[*i];
-	(*i)++;
-	while (s[*i] && s[*i] != quote)
-		(*i)++;
-	if (s[*i] == quote)
-		(*i)++;
-	count = 1;
-	return (count);
-}
-
 static int	count_args(const char *s)
 {
-	int	i;
-	int	count;
+	int		i;
+	int		count;
+	bool	in_word;
+	char	quote;
 
 	i = 0;
 	count = 0;
+	in_word = false;
+	quote = 0;
 	while (s[i])
 	{
 		skip_spaces(s, &i);
 		if (!s[i])
 			break ;
+		in_word = true;
 		if (s[i] == '\'' || s[i] == '\"')
-			count += count_quoted_token(s, &i);
+		{
+			quote = s[i++];
+			while (s[i] && s[i] != quote)
+				i++;
+			if (s[i] == quote)
+				i++;
+		}
 		else
 		{
 			while (s[i] && s[i] != ' ' && s[i] != '\t' && s[i] != '\''
 				&& s[i] != '\"')
 				i++;
-			count++;
 		}
+		count++;
 		skip_spaces(s, &i);
 	}
 	return (count);
 }
 
-static int	extract_token(const char *s, int *i, char **res)
+static char	*extract_token(const char *s, int *i)
 {
-	int		start;
-	int		end;
-	int		j;
 	char	quote;
-	char	*str;
+	char	*res;
 
-	j = 0;
+	int start, end, j = 0;
 	quote = 0;
 	skip_spaces(s, i);
 	if (s[*i] == '\'' || s[*i] == '\"')
 	{
 		quote = s[*i];
-		start = *i + 1;
+		start = ++(*i);
 		end = start;
 		while (s[end] && s[end] != quote)
 			end++;
-		str = (char *)malloc((end - start) + 1);
-		if (!str)
-			return (0);
-		while (*i + 1 < end + 1)
-			str[j++] = s[++(*i)];
-		str[j] = '\0';
-		(*i)++;
+		res = (char *)malloc(end - start + 1);
+		if (!res)
+			return (NULL);
+		while (*i < end)
+			res[j++] = s[(*i)++];
+		res[j] = 0;
+		if (s[*i] == quote)
+			(*i)++;
 	}
 	else
 	{
 		start = *i;
-		end = start;
-		while (s[end] && s[end] != ' ' && s[end] != '\t' && s[end] != '\''
-			&& s[end] != '\"')
-			end++;
-		str = (char *)malloc((end - start) + 1);
-		if (!str)
-			return (0);
-		while (*i < end)
-			str[j++] = s[(*i)++];
-		str[j] = '\0';
+		while (s[*i] && s[*i] != ' ' && s[*i] != '\t' && s[*i] != '\''
+			&& s[*i] != '\"')
+			(*i)++;
+		res = (char *)malloc(*i - start + 1);
+		if (!res)
+			return (NULL);
+		while (start < *i)
+			res[j++] = s[start++];
+		res[j] = 0;
 	}
-	*res = str;
-	return (1);
+	return (res);
 }
 
 char	**mini_shell_split(const char *s)
@@ -99,19 +101,17 @@ char	**mini_shell_split(const char *s)
 	char	**out;
 	int		i;
 	int		k;
-	int		ok;
 
 	num = count_args(s);
 	out = (char **)malloc((num + 1) * sizeof(char *));
 	i = 0;
 	k = 0;
-	ok = 1;
 	if (!out)
 		return (NULL);
-	while (k < num && ok)
+	while (k < num)
 	{
-		ok = extract_token(s, &i, &out[k]);
-		if (!ok)
+		out[k] = extract_token(s, &i);
+		if (!out[k])
 		{
 			while (k > 0)
 				free(out[--k]);
