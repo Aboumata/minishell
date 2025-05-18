8f8a25fd6210ce0bd0ba8bbd0700a4329bb52d16
aboumata <boumatabibon@gmail.com>
2025-05-18 12:46:01 +0100

    minishell

--- expander.h
+++ expander.h
@@ -24,7 +24,7 @@ typedef struct s_exp_ctx
 	int			*j;
 	int			idx;
 	int			*i;
-}	t_exp_ctx;
+}				t_exp_ctx;
 
 char			*get_var_value(const char *var, t_envs *env, int last_status);
 int				expand_question(t_exp_ctx *ctx);
@@ -32,6 +32,7 @@ int				expand_name(t_exp_ctx *ctx);
 int				replace_variable(t_exp_ctx *ctx);
 int				is_var_char(char c, int first);
 int				write_value(const char *val, char *out, int *j);
-char			*expand_variables(const char *input, t_envs *env, int last_status);
+char			*expand_variables(const char *input, t_envs *env,
+					int last_status);
 
 #endif
