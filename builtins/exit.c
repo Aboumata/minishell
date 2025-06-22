7c5cecdf57f338878f90b08cf4479a7cf0a07380
aboumata <boumatabibon@gmail.com>
2025-06-22 20:45:36 +0100

    minishell

--- builtins/exit.c
+++ builtins/exit.c
@@ -11,92 +11,107 @@
 /* ************************************************************************** */
 
 #include "../minishell.h"
+#include <limits.h>
+#include <errno.h>
 
-static int	is_valid_number(const char *str)
+static int is_valid_number(const char *str)
 {
-	int	i;
-	int	sign;
+    int i;
 
-	if (!str || *str == '\0')
-		return (0);
-	i = 0;
-	sign = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	if (str[i] == '+' || str[i] == '-')
-	{
-		sign = 1;
-		i++;
-	}
-	if (!ft_isdigit(str[i]))
-		return (0);
-	while (str[i])
-	{
-		if (!ft_isdigit(str[i]))
-		{
-			while (str[i] == ' ' || str[i] == '\t')
-				i++;
-			return (str[i] == '\0');
-		}
-		i++;
-	}
-	return (1);
+    if (!str || *str == '\0')
+        return (0);
+
+    i = 0;
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+
+    if (str[i] == '+' || str[i] == '-')
+        i++;
+
+    if (!ft_isdigit(str[i]))
+        return (0);
+    while (str[i])
+    {
+        if (!ft_isdigit(str[i]))
+        {
+            while (str[i] == ' ' || str[i] == '\t')
+                i++;
+            return (str[i] == '\0');
+        }
+        i++;
+    }
+    return (1);
 }
 
-static long long	ft_atoll(const char *str)
+static int ft_atoll_safe(const char *str, long long *result)
 {
-	long long	result;
-	int			sign;
-	int			i;
+    long long num;
+    int sign;
+    int i;
 
-	result = 0;
-	sign = 1;
-	i = 0;
-	while (str[i] == ' ' || str[i] == '\t')
-		i++;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (str[i] == '+')
-		i++;
-	while (ft_isdigit(str[i]))
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	return (result * sign);
+    num = 0;
+    sign = 1;
+    i = 0;
+
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+
+    if (str[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (str[i] == '+')
+        i++;
+
+    while (ft_isdigit(str[i]))
+    {
+        int digit = str[i] - '0';
+        if (sign == 1)
+        {
+            if (num > (LLONG_MAX - digit) / 10)
+                return (0);
+        }
+        else
+        {
+            if (num > (LLONG_MAX - digit) / 10)
+                return (0);
+        }
+        num = num * 10 + digit;
+        i++;
+    }
+
+    *result = num * sign;
+    return (1);
 }
 
-int	builtin_exit(char **argv)
+int builtin_exit(char **argv)
 {
-	long long	code;
+    long long code;
 
-	printf("exit\n");
-	if (!argv[1])
-	{
-		free_env(g_env);
-		exit(0);
-	}
-	if (!is_valid_number(argv[1]))
-	{
-		printf("minishell: exit: %s: numeric argument required\n", argv[1]);
-		free_env(g_env);
-		exit(2);
-	}
-	if (argv[2])
-	{
-		printf("minishell: exit: too many arguments\n");
-		return (1);
-	}
-	code = ft_atoll(argv[1]);
-	if (code < 0 || code > 255)
-	{
-		printf("minishell: exit: %s: numeric argument required\n", argv[1]);
-		free_env(g_env);
-		exit(2);
-	}
-	free_env(g_env);
-	exit((unsigned char)code);
+    printf("exit\n");
+    if (!argv[1])
+    {
+        free_env(g_env);
+        exit(0);
+    }
+    if (!is_valid_number(argv[1]))
+    {
+        printf("minishell: exit: %s: numeric argument required\n", argv[1]);
+        free_env(g_env);
+        exit(2);
+    }
+    if (argv[2])
+    {
+        printf("minishell: exit: too many arguments\n");
+        return (1);
+    }
+    if (!ft_atoll_safe(argv[1], &code))
+    {
+        printf("minishell: exit: %s: numeric argument required\n", argv[1]);
+        free_env(g_env);
+        exit(2);
+    }
+    free_env(g_env);
+    exit((unsigned char)code);
 }
