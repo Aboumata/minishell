0d3ec5938c0f9cea70c2409a373d8631e8edc395
aboumata <boumatabibon@gmail.com>
2025-06-30 20:28:31 +0100

    minishell

--- parsing/mini_shell_split_1.c
+++ parsing/mini_shell_split_1.c
@@ -12,12 +12,43 @@
 
 #include "parsing.h"
 
-static char	*extract_unquoted_token(const char *s, int *i)
+static char	*extract_token(const char *s, int *i)
 {
 	char	quote;
+	int		special_len;
 	char	*res;
 
-	int (start), (len);
+	int start, len;
+	quote = 0;
+	skip_spaces(s, i);
+	if (!s[*i])
+		return (NULL);
+	if ((special_len = is_special(s, *i)))
+	{
+		res = malloc(special_len + 1);
+		if (!res)
+			return (NULL);
+		ft_memcpy(res, &s[*i], special_len);
+		res[special_len] = 0;
+		*i += special_len;
+		return (res);
+	}
+	if (s[*i] == '\'' || s[*i] == '"')
+	{
+		quote = s[(*i)++];
+		start = *i;
+		while (s[*i] && s[*i] != quote)
+			(*i)++;
+		len = *i - start;
+		res = malloc(len + 1);
+		if (!res)
+			return (NULL);
+		ft_memcpy(res, &s[start], len);
+		res[len] = 0;
+		if (s[*i] == quote)
+			(*i)++;
+		return (res);
+	}
 	start = *i;
 	while (s[*i] && !is_whitespace(s[*i]) && !is_special(s, *i))
 	{
@@ -30,7 +61,9 @@ static char	*extract_unquoted_token(const char *s, int *i)
 				(*i)++;
 		}
 		else
+		{
 			(*i)++;
+		}
 	}
 	len = *i - start;
 	res = malloc(len + 1);
@@ -41,33 +74,18 @@ static char	*extract_unquoted_token(const char *s, int *i)
 	return (res);
 }
 
-static char	*extract_token(const char *s, int *i)
-{
-	char	*res;
-
-	skip_spaces(s, i);
-	if (!s[*i])
-		return (NULL);
-	res = extract_special_token(s, i);
-	if (res)
-		return (res);
-	res = extract_quoted_token(s, i);
-	if (res)
-		return (res);
-	return (extract_unquoted_token(s, i));
-}
-
 static int	count_args(const char *s)
 {
 	int		i;
 	char	*tok;
+	int		save_i;
 	int		count;
 
 	i = 0;
-	i = 0;
-	count = 0;
+	i = 0, count = 0;
 	while (1)
 	{
+		save_i = i;
 		tok = extract_token(s, &i);
 		if (!tok)
 			break ;
@@ -82,7 +100,7 @@ char	**mini_shell_split(const char *s)
 	char	**out;
 	char	*tok;
 
-	int (num), (i), (k);
+	int(num), i, k;
 	num = count_args(s);
 	out = malloc((num + 1) * sizeof(char *));
 	if (!out)
