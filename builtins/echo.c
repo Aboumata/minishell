3888cf2ce2921b3e0272ca19ea42bb6fddc2b6e5
aboumata <boumatabibon@gmail.com>
2025-07-10 21:21:22 +0100

    minishell

--- builtins/echo.c
+++ builtins/echo.c
@@ -22,7 +22,7 @@ int	builtin_echo(char **argv)
 	while (argv[i] && argv[i][0] == '-' && argv[i][1] == 'n')
 	{
 		j = 2;
-		while (argv[i][j] == 'n')
+		while (argv[i][j] == 'n' || argv[i][j] == 'E' || argv[i][j] == 'e')
 			j++;
 		if (argv[i][j] != '\0')
 			break ;
